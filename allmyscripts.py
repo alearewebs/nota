5b723a2c40e96ff8e9cdcb5f90d8948c1367d06e
AlemakesWebs <alejandro.itzae.arellano@gmail.com>
2026-03-14 16:49:12 -0700

    Add files via upload

---
+++ allmyscripts.py
@@ -0,0 +1,23 @@
+age = 12
+print(age)
+
+print('Hello, World!')
+person = input ('What is your name?')
+print('Hello, ', person)
+
+a = 6
+b = a * 7
+print(b)
+
+c = 10
+print(b)
+
+f = c * 7
+print(f)
+
+name = ' Ally Alien'
+print(name)
+
+greeting = 'Welcome to Earth,'
+message = greeting +  name
+print(message)
